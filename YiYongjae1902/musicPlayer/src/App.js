8116123085992e9487b255bf70bfe3cbe978de45
이용재 <dididy@knou.ac.kr>
2019-09-27 12:19:04 +0900

    Add: tabview icon on each section

--- YiYongjae1902/musicPlayer/src/App.js
+++ YiYongjae1902/musicPlayer/src/App.js
@@ -8,6 +8,8 @@
 
 import React, {Component} from 'react';
 import {Actions, Router, Scene, Tabs, Lightbox} from 'react-native-router-flux';
+import Icon from 'react-native-ionicons';
+
 import ForYou from './screens/ForYou';
 import Main from './screens/Main';
 import Player from './screens/Player';
@@ -15,20 +17,40 @@ import MiniPlayer from './screens/MiniPlayer';
 import AlbumScreen from './screens/AlbumScreen';
 import PlaylistScreen from './screens/PlaylistScreen';
 
+const musicIcon = () => (
+  <Icon name="musical-notes" style={{fontSize: 20, color: 'gray'}} />
+);
+const playlistIcon = () => (
+  <Icon name="list" style={{fontSize: 20, color: 'gray'}} />
+);
+const albumIcon = () => (
+  <Icon name="albums" style={{fontSize: 20, color: 'gray'}} />
+);
+const heartIcon = () => (
+  <Icon name="heart" style={{fontSize: 20, color: 'gray'}} />
+);
 export default class App extends Component {
   render() {
     return (
       <Router hideNavBar="true">
         <Lightbox>
           <Tabs>
-            <Scene title="보관함" key="main" component={Main} initial={true} />
-            <Scene title="Playlist" key="Playlist" component={PlaylistScreen} />
-            <Scene title="Album" key="Album" component={AlbumScreen} />
+            <Scene
+              title="보관함"
+              key="main"
+              component={Main}
+              initial={true}
+              icon={musicIcon}
+            />
+            <Scene title="Playlist" key="Playlist" component={PlaylistScreen} icon={playlistIcon} />
+            <Scene title="Album" key="Album" component={AlbumScreen} icon={albumIcon}/>
             <Scene
               title="ForYou"
               key="foryou"
               component={ForYou}
               initial={false}
+              icon={heartIcon}
+
             />
           </Tabs>
           <Scene key="popPlayer" component={Player} />
