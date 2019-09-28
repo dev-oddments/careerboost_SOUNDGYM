35e1982e79daec94cfad5ef9a1681eda4ef58687
이용재 <dididy@knou.ac.kr>
2019-09-28 17:17:26 +0900

    Add: modal animation

--- YiYongjae1902/musicPlayer/src/screens/Player.js
+++ YiYongjae1902/musicPlayer/src/screens/Player.js
@@ -1,5 +1,13 @@
 import React from 'react';
-import {Image, View, StyleSheet, TouchableOpacity, Slider} from 'react-native';
+import {
+  Image,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Slider,
+  Dimensions,
+  Animated,
+} from 'react-native';
 import Icon from 'react-native-ionicons';
 import {Text, Button} from 'native-base';
 import {Actions} from 'react-native-router-flux';
@@ -8,18 +16,33 @@ import TrackPlayer from 'react-native-track-player';
 import ImageResize from '../components/ImageResize';
 import ProgressNow from '../components/ProgressNow';
 
+const {height: deviceHeight, width: deviceWidth} = Dimensions.get('window');
+
 export default class player extends TrackPlayer.ProgressComponent {
   state = {
     Volume: 60,
     isPlay: 'pause',
+    offset: new Animated.Value(deviceHeight),
   };
 
   componentDidMount() {
+    Animated.timing(this.state.offset, {
+      duration: 100,
+      toValue: 0,
+    }).start();
+
     TrackPlayer.setupPlayer().then(async () => {
       TrackPlayer.reset();
       TrackPlayer.add(this.props.data);
       TrackPlayer.play();
     });
+  }
+
+  _closeModal() {
+    Animated.timing(this.state.offset, {
+      duration: 150,
+      toValue: deviceHeight,
+    }).start(Actions.pop);
   }
 
   _togglePlay() {
@@ -44,73 +67,75 @@ export default class player extends TrackPlayer.ProgressComponent {
 
   render() {
     return (
-      <View style={styles.baseContainer}>
-        <View style={styles.closeButton}>
-          <TouchableOpacity
-            onPress={() => {
-              Actions.MinimizePlayer({data: this.props.data, isPlay: this.state.isPlay});
-            }}>
-            <Icon name="arrow-down" style={{fontSize: 45, color: 'gray'}} />
-          </TouchableOpacity>
+      <Animated.View style={{transform: [{translateY: this.state.offset}]}}>
+        <View style={styles.baseContainer}>
+          <View style={styles.closeButton}>
+            <TouchableOpacity
+              onPress={() => {
+                this._closeModal();
+              }}>
+              <Icon name="arrow-down" style={{fontSize: 45, color: 'gray'}} />
+            </TouchableOpacity>
+          </View>
+          <View style={styles.image}>
+            <ImageResize
+              img={this.props.data.artwork}
+              isPlay={this.state.isPlay}
+            />
+          </View>
+          <ProgressNow />
+          <View style={styles.info}>
+            <Text style={{fontSize: 25, fontWeight: 'bold'}}>
+              {this.props.data.title}
+            </Text>
+            <Text style={{fontSize: 20, color: 'rgb(218,67,84)'}}>
+              {this.props.data.artist}
+            </Text>
+          </View>
+          <View style={styles.controller}>
+            <TouchableOpacity onPress={() => this._getRewind()}>
+              <Icon name="rewind" style={{fontSize: 40}} />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => this._togglePlay()}>
+              <Icon name={this.state.isPlay} style={{fontSize: 55}} />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => TrackPlayer.skipToNext()}>
+              <Icon name="fastforward" style={{fontSize: 40}} />
+            </TouchableOpacity>
+          </View>
+          <View style={styles.volume}>
+            <Icon name="volume-mute" style={{fontSize: 20}} />
+            <Slider
+              minimumValue={0}
+              maximumValue={100}
+              thumbTintColor="white"
+              thumbStyle={styles.thumbShadow}
+              minimumTrackTintColor="#000000"
+              maximumTrackTintColor="#808080"
+              step={1}
+              onValueChange={val => {
+                this.setState({Volume: val});
+                TrackPlayer.setVolume(val / 100);
+              }}
+              onSlidingComplete={val => {
+                this.setState({Volume: val});
+                TrackPlayer.setVolume(val / 100);
+              }}
+              style={{width: '90%'}}
+              value={this.state.Volume}
+            />
+            <Icon name="volume-high" style={{fontSize: 20}} />
+          </View>
         </View>
-        <View style={styles.image}>
-          <ImageResize
-            img={this.props.data.artwork}
-            isPlay={this.state.isPlay}
-          />
-        </View>
-        <ProgressNow />
-        <View style={styles.info}>
-          <Text style={{fontSize: 25, fontWeight: 'bold'}}>
-            {this.props.data.title}
-          </Text>
-          <Text style={{fontSize: 20, color: 'rgb(218,67,84)'}}>
-            {this.props.data.artist}
-          </Text>
-        </View>
-        <View style={styles.controller}>
-          <TouchableOpacity onPress={() => this._getRewind()}>
-            <Icon name="rewind" style={{fontSize: 40}} />
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => this._togglePlay()}>
-            <Icon name={this.state.isPlay} style={{fontSize: 55}} />
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => TrackPlayer.skipToNext()}>
-            <Icon name="fastforward" style={{fontSize: 40}} />
-          </TouchableOpacity>
-        </View>
-        <View style={styles.volume}>
-          <Icon name="volume-mute" style={{fontSize: 20}} />
-          <Slider
-            minimumValue={0}
-            maximumValue={100}
-            thumbTintColor="white"
-            thumbStyle={styles.thumbShadow}
-            minimumTrackTintColor="#000000"
-            maximumTrackTintColor="#808080"
-            step={1}
-            onValueChange={val => {
-              this.setState({Volume: val});
-              TrackPlayer.setVolume(val / 100);
-            }}
-            onSlidingComplete={val => {
-              this.setState({Volume: val});
-              TrackPlayer.setVolume(val / 100);
-            }}
-            style={{width: '90%'}}
-            value={this.state.Volume}
-          />
-          <Icon name="volume-high" style={{fontSize: 20}} />
-        </View>
-      </View>
+      </Animated.View>
     );
   }
 }
 
 const styles = StyleSheet.create({
   baseContainer: {
-    flex: 11,
-
+    height: deviceHeight * 0.9,
+    width: deviceWidth * 1,
     shadowColor: '#000',
     shadowOffset: {
       width: 0,
