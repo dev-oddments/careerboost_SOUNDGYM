ec93881e658bfb1f13c66ea99a2ff606acec79de
이용재 <dididy@knou.ac.kr>
2019-09-29 21:35:15 +0900

    Fix: android, ios build files

--- YiYongjae1902/musicPlayer/src/PageThree.js
+++
@@ -1,4 +0,0 @@
-import React, { Component } from 'react';
-import { Container, Content, Text, Card, Header, Body, Button, Title, CardItem } from 'native-base';
-import TrackPlayer from 'react-native-track-player';
-
